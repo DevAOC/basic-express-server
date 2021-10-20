905f8c74fa62a19229270dc032eebf7aeae7ad8b
AntoineCharette <antoineocharette@gmail.com>
2021-10-20 17:55:56 -0400

    changes code to follow what Jacob and I did in code review

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -1,14 +1,15 @@
 'use strict';
 
 const validator = require('../lib/middleware/validator.js');
-const error = require('../lib/error-handlers/500.js');
 
 describe('Testing the validator middleware', () => {
+  let req = { method: 'GET', query: {} };
+  let res = { status: jest.fn() };
+  let next = jest.fn();
   it('Should throw an error if there is no name property', () => {
     req.query.name = undefined;
     validator(req, res, next);
 
-    expect(next).toHaveBeenCalledWith(error);
-    expect(res.status).toBe(500);
+    expect(next).toHaveBeenCalledWith('Invalid Request');
   });
 });
